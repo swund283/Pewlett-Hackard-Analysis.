ee119f38a2b2cb622f6cd53f2714a7f0bb74fddb
sam wunderilch <swund@MacBook-Pro.local>
2020-07-05 10:40:42 -0400

    updtes in challenge

---
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Data/Challenge.sql
@@ -0,0 +1,8 @@
+create table silver_tsunami_info (
+	emp_no INT NOT NULL,
+    first_name varchar NOT NULL,
+	title varchar NOT NULL,
+	from_date DATE NOT NULL,
+	salary INT NOT NULL,
+	birth_date Date not null
+);
